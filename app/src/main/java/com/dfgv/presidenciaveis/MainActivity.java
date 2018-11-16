39f411f00b5a92d2f4e76e7bd5c82d62d7533d4a
Guilherme <guilherme.freitas@poggers.com.br>
2018-11-16 19:23:26 -0200

    Main
    adicionado código da main

--- app/src/main/java/com/dfgv/presidenciaveis/MainActivity.java
+++ app/src/main/java/com/dfgv/presidenciaveis/MainActivity.java
@@ -1,7 +1,11 @@
 package com.dfgv.presidenciaveis;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -9,6 +13,23 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+
+//        int orientation = getResources().getConfiguration().orientation;
+//
+//        if (orientation == Configuration.ORIENTATION_PORTRAIT){
+//            Intent i1 = new Intent(this, Main2Activity.class);
+//            startActivity(i1);
+//        }
+//            else{
+//            // TODO Auto-generated method stub
+//            super.onRestart();
+//            Intent i = new Intent(MainActivity.this, MainActivity.class);  //your class
+//            startActivity(i);
+//            finish();
+//        }
 
     }
+
 }
