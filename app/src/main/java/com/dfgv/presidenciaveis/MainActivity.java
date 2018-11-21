c9048e1ab33ae32bb51b6f3fb0202dd079cbd732
Guilherme <guilherme.freitas@poggers.com.br>
2018-11-21 19:53:14 -0200

    Tela main
    Arrumei a tela main

--- app/src/main/java/com/dfgv/presidenciaveis/MainActivity.java
+++ app/src/main/java/com/dfgv/presidenciaveis/MainActivity.java
@@ -1,11 +1,8 @@
 package com.dfgv.presidenciaveis;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.app.Activity;
 
 public class MainActivity extends Activity {
 
@@ -14,22 +11,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
-
-//        int orientation = getResources().getConfiguration().orientation;
-//
-//        if (orientation == Configuration.ORIENTATION_PORTRAIT){
-//            Intent i1 = new Intent(this, Main2Activity.class);
-//            startActivity(i1);
-//        }
-//            else{
-//            // TODO Auto-generated method stub
-//            super.onRestart();
-//            Intent i = new Intent(MainActivity.this, MainActivity.class);  //your class
-//            startActivity(i);
-//            finish();
-//        }
-
     }
 
 }
