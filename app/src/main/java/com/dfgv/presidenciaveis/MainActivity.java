a34b786aaa9584c743558c25e4431535d6442b5d
Guilherme <guilherme.freitas@poggers.com.br>
2018-11-22 12:03:05 -0200

    Regra do jogo
    Adicionei a regra do jogo

--- app/src/main/java/com/dfgv/presidenciaveis/MainActivity.java
+++ app/src/main/java/com/dfgv/presidenciaveis/MainActivity.java
@@ -6,12 +6,13 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 
     Button jogar;
-
+    TextView regras;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,12 +20,20 @@ public class MainActivity extends Activity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         jogar = findViewById(R.id.button);
+        regras = findViewById(R.id.regrajogo);
 
 
         jogar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), PopUpVotar.class);
+                startActivity(i);
+            }
+        });
+        regras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), regras.class);
                 startActivity(i);
             }
         });
---
+++ app/src/main/java/com/dfgv/presidenciaveis/regras.java
@@ -0,0 +1,25 @@
+package com.dfgv.presidenciaveis;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import static android.text.Layout.JUSTIFICATION_MODE_INTER_WORD;
+
+public class regras extends Activity {
+
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regras);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        TextView textView = (TextView) findViewById(R.id.textView5) ;
+
+        textView.setJustificationMode(JUSTIFICATION_MODE_INTER_WORD);
+
+    }
+}
