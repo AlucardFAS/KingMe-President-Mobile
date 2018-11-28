50e71bbdeeed19d87de0100e554c4096ea119818
Guilherme <guilherme.freitas@poggers.com.br>
2018-11-28 20:20:23 -0200

    Fontes e cor
    Alterei as fontes e a cor na tela de placar

--- app/src/main/java/com/dfgv/presidenciaveis/placar.java
+++ app/src/main/java/com/dfgv/presidenciaveis/placar.java
@@ -18,20 +18,23 @@ public class placar extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_placar);
         mensagens = (ViewGroup) findViewById(R.id.container);
-        addItem("Título 1", "Exemplo de mensagem.");
-        addItem("Título 2", "Exemplo de mensagem.");
-        addItem("Título 3", "Exemplo de mensagem.");
-        addItem("Título 4", "Exemplo de mensagem.");
-        addItem("Título 5", "Exemplo de mensagem.");
+        addItem("Título 1", 2, 1);
+        addItem("Título 2", 2, 1);
+        addItem("Título 3", 2, 1);
+        addItem("Título 4", 2, 1);
+        addItem("Título 5", 2, 1);
 
     }
-    private void addItem(String textoDoTitulo, String textoDaMensagem) {
-        CardView cardView= (CardView) LayoutInflater.from(this).inflate(R.layout.card, mensagens, false);
-        TextView titulo = (TextView) cardView.findViewById(R.id.titulo);
-        TextView mensagem = (TextView) cardView.findViewById(R.id.mensagem);
-        titulo.setText("Nome: "+textoDoTitulo);
-        mensagem.setText("Pontuação: "+textoDaMensagem);
-        mensagens.addView(cardView);
+    private void addItem(String nome, int pontuacaopartida, int pontuacaojogo) {
+        CardView cardView = (CardView) LayoutInflater.from(this)
+                .inflate(R.layout.card, mensagens, false);
+        TextView nomes = (TextView) cardView.findViewById(R.id.nome);
+        TextView pontuacaorodadas = (TextView) cardView.findViewById(R.id.pontrodada);
+        TextView ponuacaojogos = (TextView) cardView.findViewById(R.id.pontjogo);
 
+        nomes.setText(nome);
+        pontuacaorodadas.setText(""+pontuacaopartida);
+        ponuacaojogos.setText(""+pontuacaojogo);
+        mensagens.addView(cardView);
     }
 }
