5a91f658f55c3f0c1feb3075ffd73828b6cfc177
Fernando A da Silva <FernandoImp@outlook.com.br>
2018-11-28 22:28:24 -0200

    pop up com borda, votar pronto

--- app/src/main/java/com/dfgv/presidenciaveis/MainActivity.java
+++ app/src/main/java/com/dfgv/presidenciaveis/MainActivity.java
@@ -4,51 +4,27 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 
     Button jogar;
-    TextView regras;
-    ImageView sobre;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        sobre = findViewById(R.id.icone);
 
         jogar = findViewById(R.id.button);
-        regras = findViewById(R.id.regrajogo);
 
-        sobre.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), sobre.class);
-                startActivity(i);
-            }
-        });
 
         jogar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent i = new Intent(getApplicationContext(), PopUpVotar.class);
-//                startActivity(i);
-                Intent i = new Intent(getApplicationContext(), placar.class);
-               startActivity(i);
-            }
-        });
-        regras.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), regras.class);
+                Intent i = new Intent(getApplicationContext(), PopUpSelecionar.class);
                 startActivity(i);
             }
         });
@@ -56,19 +32,5 @@ public class MainActivity extends Activity {
 
 
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
 
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.sobre) {
-            Intent intent = new Intent(this, sobre.class);
-            startActivity(intent);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/com/dfgv/presidenciaveis/PopUpSelecionar.java
+++ app/src/main/java/com/dfgv/presidenciaveis/PopUpSelecionar.java
@@ -3,45 +3,21 @@ package com.dfgv.presidenciaveis;
 import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 
-public class PopUpVotar extends Activity {
-
-    Button btnClose;
-    Button btnYes;
+public class PopUpSelecionar extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop_up_selecionar);
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);//aqui apaga as parada de título da janela do PopUp
-
-        setContentView(R.layout.activity_pop_up_votar);
-
-        btnClose = findViewById(R.id.btnCancelar);
-        btnClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
-        btnYes = findViewById(R.id.btnSim);
-        btnYes.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Retornar true
-            }
-        });
-
-
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -49,13 +25,14 @@ public class PopUpVotar extends Activity {
         int width = dm.widthPixels;
         int height = dm.heightPixels;
 
-        getWindow().setLayout((int)(width*.85), (int)(height*.50));
+        getWindow().setLayout((int)(width*.85), (int)(height*.70));
 
         WindowManager.LayoutParams params = getWindow().getAttributes();
         params.gravity = Gravity.CENTER;
         params.x = 0;
         params.y = -20;
-        getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT)); // Aqui tira os esqueminhas de borda
+        getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         getWindow().setAttributes(params);
+
     }
 }
